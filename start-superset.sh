a4026882fa71071aa4b7a246db754260cb90dd96
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2024-09-17 09:40:25 -0300

    Fix start-superset.sh

--- start-superset.sh
+++ start-superset.sh
@@ -1,2 +1,2 @@
-docker compose -f ./superset/docker-compose.yaml                                                                                                                                                                                                                 down | true
+docker compose -f ./superset/docker-compose.yaml down | true
 docker compose -f ./superset/docker-compose.yaml up -d $1
